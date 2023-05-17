7c8dbd0023f33ec15ea7af896a22ec7478d6d586
nyckolas <nycparroseixas@gmail.com>
2023-05-17 09:42:22 -0300

    12 ao 14

---
+++ 5_op_expressoes/13_op_igualdade/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    if(5 == 5){
+        echo "Comparação verdadeira 1 <br>";
+    }
+
+    if(3 == 4){
+        echo "Comparação verdadeira 2 <br>";
+    }
+
+$a = 12;
+$b = 12;
+$c = 100;
+
+    if($a == $b){
+        echo "Comparação verdadeira 3 <br>";
+    }
+
+    if($a == $c){
+        echo "Comparação verdadeira 4 <br>";
+    }
+
+$nome = "abc";
+$nome_do_sistema = "cba";
+
+    if($nome == $nome_do_sistema){
+        echo "Os nomes coincidem 1 <br>";
+    }
+
+    if($nome == $nome_do_sistema){
+        echo "Os nomes coincidem 2 <br>";
+    }
---
+++ 5_op_expressoes/14_exercicio_15/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 10;
+$b = 10;
+$c = 20;
+
+if($a == $b){
+    echo "Verdadeiro <br>";
+}
+
+if($a != $c){
+    echo "Falso <br>";
+}
